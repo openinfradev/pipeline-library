9c0fa853d504d7a7af660158658f32804b741b84
Robert Choi <taeilchoi1@gmail.com>
2020-09-15 13:34:49 +0900

    modify fetchCloudsConf() function

--- fetchCloudsConf.groovy
+++ fetchCloudsConf.groovy
@@ -1,11 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-  println "Fetching cloud config from repository..."
   exists = fileExists('./clouds.yaml')
   if (! exists) {
-    // TO-DO: add routine to fetch clolds.yaml
-
-
+    sh "cp gate/clouds.yaml ."
   }
 }
