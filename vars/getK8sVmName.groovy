627402324db2b80c021bc81f54d306e9907d5422
Robert Choi <taeilchoi1@gmail.com>
2020-12-04 17:33:24 +0900

    getK8sVmName(): check if retrieved key is correct

--- vars/getK8sVmName.groovy
+++ vars/getK8sVmName.groovy
@@ -2,11 +2,11 @@
 
 def call(String prefix) {
   values = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get --prefix ${prefix} | head -n 2").trim().split('\n')
-  // check first line & second line
-  if (values[0].startsWith(prefix) && values[1] ) {
+  // check if retrieved key is correct
+  if (values[0].startsWith(prefix) && values[0].contains("vmName") && values[1] ) {
     print("Retrieved vmName: ${values[1]} from key: ${values[0]}")
   } else {
-    error("Error getting k8s cluster endpoint from etcd")
+    error("Error getting k8s VM name from etcd. Retrieved key: ${values[0]}")
   }
 
   return values
