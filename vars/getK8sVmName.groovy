6bf451c6343643ecefdde602c5d7b1c6e4ab8295
Robert Choi <taeilchoi1@gmail.com>
2020-12-15 11:48:34 +0900

    getK8sVmName(): use json format

--- vars/getK8sVmName.groovy
+++ vars/getK8sVmName.groovy
@@ -1,13 +1,20 @@
 #!/usr/bin/env groovy
 
 def call(String prefix) {
-  values = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get --prefix ${prefix} | head -n 2").trim().split('\n')
+  output = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get --prefix ${prefix} -w json").trim()
+
+  def slurper = new JsonSlurper()
+  def result = slurper.parseText(output)
+
+  vmKey = result.kvs[0].key.decodeBase64()
+  vmVal = result.kvs[0].value.decodeBase64()
+
   // check if retrieved key is correct
-  if (values[0].startsWith(prefix) && values[0].contains("vmName") && values[1] ) {
-    println("Retrieved vmName: ${values[1]} from key: ${values[0]}")
+  if (vmKey.startsWith(prefix) && vmVal ) {
+    println("Retrieved vmName: ${vmVal} from key: ${vmKey}")
   } else {
-    error("Error getting k8s VM name from etcd. Retrieved key: ${values[0]}")
+    error("Error getting k8s VM name from etcd. Retrieved key: ${vmKey}")
   }
 
-  return values
+  return vmVal
 }
