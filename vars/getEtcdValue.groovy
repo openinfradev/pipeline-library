893a68a2d4299bbf60f1f00188b8c4533c0d6959
Robert Choi <taeilchoi1@gmail.com>
2020-12-15 11:27:50 +0900

    getEtcdValue(): use etcdctl instead of curl cmd

--- vars/getEtcdValue.groovy
+++ vars/getEtcdValue.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String dir, String key) {
-  //value = sh(returnStdout: true, script: "curl -s ${env.ETCD_URL}/${dir}/${key} | jq -r .node.value").trim()
-  value = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get ${key}").trim()
-  return value 
+  value = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get ${dir}/${key}").trim()
+  return value
 }
