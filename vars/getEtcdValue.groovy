43d3bfeaf17e8fc1d1fc29d61da81eeef933f972
Robert Choi <taeilchoi1@gmail.com>
2021-01-27 11:25:26 +0900

    use etcd in pangyo infra

--- vars/getEtcdValue.groovy
+++ vars/getEtcdValue.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String dir, String key) {
-  value = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL} get ${dir}/${key}").trim()
+  value = sh(returnStdout: true, script: "etcdctl --endpoints ${env.ETCD_URL_PANGYO} get ${dir}/${key}").trim()
   return value
 }
