3423360d72437fe0ab1583cca43d135825f8f6bf
Robert Choi <taeilchoi1@gmail.com>
2021-01-28 11:40:41 +0900

    getOpenstackVMinfo(): add null check

--- vars/fetchCloudsConf.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-  exists = fileExists('./clouds.yaml')
-  if (! exists) {
-    sh "cp gate/clouds.yaml ."
-  }
-}
--- vars/getOpenstackVMinfo.groovy
+++ vars/getOpenstackVMinfo.groovy
@@ -1,17 +1,19 @@
 #!/usr/bin/env groovy
 def call(String namePrefix, String networkType="mgmt", String provider='taco-prod') {
-  fetchCloudsConf()
-
 	def result = [:]
 	infos = sh(returnStdout: true, script: "openstack server list --os-cloud ${provider} -f json | jq --raw-output 'sort_by(.Name) | .[] | select(.Name|test(\"${namePrefix}\")) | .Name, .Networks'").split('\n')
 	print infos
-	for (i = 0; i < infos.size(); i=i+2) {
-	  infos[i+1].split(";").each { it ->
-      if(it.contains(networkType)) {
-				ip = it.split("=")[-1]
-	  		result[infos[i]] = ip
-			}
-		}
-	}
+  if(infos) {
+    for (i = 0; i < infos.size(); i=i+2) {
+      infos[i+1].split(";").each { it ->
+        if(it.contains(networkType)) {
+          ip = it.split("=")[-1]
+          result[infos[i]] = ip
+        }
+      }
+    }
+  } else {
+    error("No VM info found!")
+  }
 	return result
 }
